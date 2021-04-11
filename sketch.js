2e59831315779dd86e0a7859422f746b7280edcf
manasvigithub123 <75515582+manasvigithub123@users.noreply.github.com>
2021-04-11 16:53:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var  database,gameState=0,playerCount;
+var form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+game=new Game()
+game.getState()
+game.start()
+  
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
